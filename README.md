20006d86dc8a7936c2922b5ef36e67467f711993
= <=>
2020-04-21 22:03:19 +0200

    clean code

--- README.md
+++ README.md
@@ -2,26 +2,26 @@
 
 CLI tool to directly zip several files/folders or an existing folder
 
-Mare a zip from list of files/folders with or without specifying the archive name ("%Y_%m_%d-%H_%M_%S.zip" if not precised) or from an existing folder
+Make a zip from list of files/folders with or without specifying the archive name ("%Y_%m_%d-%H_%M_%S.zip" if not specified) or from an existing folder.
 
 
-# installation
+# Installation
 ```sh
-with pip:
+With pip:
 sudo pip3 install zipfs
 
-with yay:
+With yay:
 yay -a zipf
 
-with yaourt:
+With yaourt:
 yaourt -a zipf
 ```
 
-# compatibility
+# Compatibility
 python >= 3
 
 
-# usage
+# Usage
 <pre>
 <b>zipf / zipper</b> [<b>F_PATH_01 F_PATH_02 ...</b>] [<b>ARCHIVE_NAME</b>]
 <b>options:</b>
@@ -29,22 +29,22 @@ python >= 3
 </pre>
 
 
-# examples
-for **help**:<br/>
+# Examples
+For **help**:<br/>
 ```sh
 zipf -h
 or
 rt --help
 ```
 
-<br/>make a zip archive from 3 files:<br/>
+<br/>Make a zip archive from 3 files:<br/>
 ```sh
 zipf titi/toto.jpg titi/tutu.jpg tata.txt
 ```
-giving the **2019_05_15-17_25_44.zip** archive<br/><br/>
+Giving the **2019_05_15-17_25_44.zip** archive<br/><br/>
 
-make a zip archive from 2 files and one folder with specifying the archive name (work):<br/>
+Make a zip archive from 2 files and one folder with specifying the archive name (work):<br/>
 ```sh
 zipf titi/toto.jpg titi/ tata.txt work
 ```
-giving the **work.zip** archive
+Giving the **work.zip** archive
--- zipf/zipf.py
+++ zipf/zipf.py
@@ -8,26 +8,23 @@ from datetime import datetime
 CBRED = '\033[38;5;196;1m'
 CBORANGE = '\033[38;5;202;1m'
 CBGREEN = '\033[38;5;40;1m'
-
 CBWHITE = '\033[1;37m'
-# CBPURPLE = '\033[1;35m'
 CBBLUE = '\033[1;34m'
-
 CBASE = '\033[0m'
 
 
-def check_help_request(args):
+def _help_requested(args):
     if len(args) == 1 and (args[0] == "-h" or args[0] == "--help"):
-        README_path = "/usr/lib/zipf/README.md"
+        readme_path = "/usr/lib/zipf/README.md"
 
-        f = open(README_path, 'r')
+        f = open(readme_path, 'r')
         print(CBBLUE + "\n\t#######      zipf documentation      #######\n" + CBWHITE)
 
         for line in f:
             if line == "```sh\n" or line == "```\n" or line == "<pre>\n" or line == "</pre>\n":
                 continue
-            line = line.replace('```sh', '').replace('```', '').replace('<pre>', '').replace('</b>', '').\
-                replace('<b>', '').replace('<!-- -->', '').replace('<br/>', '').replace('```sh', '').\
+            line = line.replace('```sh', '').replace('```', '').replace('<pre>', '').replace('</b>', ''). \
+                replace('<b>', '').replace('<!-- -->', '').replace('<br/>', '').replace('```sh', ''). \
                 replace('***', '').replace('***', '').replace('**', '').replace('*', '')
 
             print(" " + line, end='')
@@ -35,79 +32,77 @@ def check_help_request(args):
         exit()
 
 
-def OK(msg=""):
+def _ok(msg=""):
     print(CBGREEN + "\n\t[OK] " + CBASE + msg)
 
 
-def INFO(msg=""):
-    print(CBWHITE + "\n\t[INFO] " + CBASE + msg)
+# def _info(msg=""):
+#     print(CBWHITE + "\n\t[INFO] " + CBASE + msg)
 
 
-def WARNING(msg=""):
+def _warning(msg=""):
     print(CBORANGE + "\n\t[WARNING] " + CBASE + msg)
 
 
-def ERROR(msg=""):
+def _error(msg=""):
     print(CBRED + "\n\t[ERROR] " + CBASE + msg)
 
 
-def skipped():
+def _skipped():
     print(CBBLUE + "\n\t\t\tskipped\n\n" + CBASE)
 
 
-def path_exists(path):
+def _path_exists(path):
     if not os.path.exists(path):
         return False
     return True
 
 
-def get_abs_path(fpath):
-    return os.path.normpath((os.path.join(os.getcwd(), os.path.expanduser(fpath))))
+def _get_abs_path(f_path):
+    return os.path.normpath((os.path.join(os.getcwd(), os.path.expanduser(f_path))))
 
 
-def error_man(init_msg, err_msg, folder_path, moved_folder_path):
-    ERROR(init_msg + " error:\n\t\t" + str(err_msg))
+def _error_man(init_msg, err_msg, folder_path, moved_folder_path):
+    _error(init_msg + " error:\n\t\t" + str(err_msg))
 
     sudo_conf = input(CBWHITE + "\n\t\tuse sudo?\n\t\t\t[Enter] to proceed\t\t[any case] to skip\n")
     if sudo_conf == "":
         subprocess.check_call(['sudo', "mv", folder_path, moved_folder_path])
     else:
-        skipped()
+        _skipped()
 
 
-def check_f_moved(folder_path, moved_folder_path):
+def _check_f_moved(folder_path, moved_folder_path):
     if os.path.exists(folder_path) or not os.path.exists(moved_folder_path):
         if os.path.exists(folder_path):
-            WARNING(CBBLUE + "%s" % folder_path + CBASE + " still exists")
+            _warning(CBBLUE + "%s" % folder_path + CBASE + " still exists")
         if not os.path.exists(moved_folder_path):
-            WARNING(CBBLUE + "%s" % moved_folder_path + CBASE + " doesn't exist")
+            _warning(CBBLUE + "%s" % moved_folder_path + CBASE + " doesn't exist")
         return False
     return True
 
 
-def check_archive_created(archive_name):
+def _check_archive_created(archive_name):
     archive_path = os.getcwd() + "/" + archive_name + ".zip"
     if os.path.isfile(archive_path):
-        OK(CBBLUE + "%s" % archive_path + CBASE + " created")
+        _ok(CBBLUE + "%s" % archive_path + CBASE + " created")
         return True
     else:
-        ERROR(CBBLUE + "%s" % archive_path + CBASE + " not created")
+        _error(CBBLUE + "%s" % archive_path + CBASE + " not created")
         return False
 
 
-def zip_folder(folder_path, archive_name=None):
-
+def _zip_folder(folder_path, archive_name=None):
     if not archive_name:
         cdatetime = datetime.now()
         archive_name = cdatetime.strftime("%Y_%m_%d-%H_%M_%S")
 
     shutil.make_archive(archive_name, 'zip', folder_path)
-    check_archive_created(archive_name)
+    _check_archive_created(archive_name)
     # shutil.rmtree(folder_path)
 
 
-def zip_files(flist, archive_name=None):
-
+def _zip_files(flist, archive_name=None):
     cdatetime = datetime.now()
     ctime = cdatetime.strftime("%Y_%m_%d-%H_%M_%S")
 
@@ -117,77 +112,78 @@ def zip_files(flist, archive_name=None):
     cpath = os.getcwd()
     folder_path = cpath + "/" + archive_name
 
-    if path_exists(folder_path):
-        WARNING(CBBLUE + "%s" % folder_path + CBASE + " already exists" + CBASE)
+    if _path_exists(folder_path):
+        _warning(CBBLUE + "%s" % folder_path + CBASE + " already exists" + CBASE)
         moved_folder_path = folder_path + "_" + ctime
         try:
             shutil.move(folder_path, moved_folder_path)
 
         except PermissionError as err_msg:
-            error_man("permission", err_msg, folder_path, moved_folder_path)
+            _error_man("permission", err_msg, folder_path, moved_folder_path)
 
         except OSError as err_msg:
-            error_man("os", err_msg, folder_path, moved_folder_path)
+            _error_man("os", err_msg, folder_path, moved_folder_path)
 
         except Exception as err_msg:
-            error_man("", err_msg, folder_path, moved_folder_path)
+            _error_man("", err_msg, folder_path, moved_folder_path)
 
-        if check_f_moved(folder_path, moved_folder_path):
-            OK(CBBLUE + "%s" % folder_path + CBASE + " moved" + CBASE)
+        if _check_f_moved(folder_path, moved_folder_path):
+            _ok(CBBLUE + "%s" % folder_path + CBASE + " moved" + CBASE)
         else:
-            ERROR("an issue occurred when moving file " + CBBLUE + "%s" % folder_path + CBASE)
+            _error("an issue occurred when moving file " + CBBLUE + "%s" % folder_path + CBASE)
             raise ValueError("not able to rename " + CBBLUE + "%s" % folder_path + CBASE)
 
     try:
         os.mkdir(folder_path)
     except Exception as err_msg:
-        ERROR("an issue occurred when creating folder " + CBBLUE + "%s" % folder_path + CBASE + "\n%s" % err_msg)
+        _error("an issue occurred when creating folder " + CBBLUE + "%s" % folder_path + CBASE + "\n%s" % err_msg)
         raise ValueError("not able to create " + CBBLUE + "%s" % folder_path + CBASE + " folder")
 
     for f in flist:
-        fpath = get_abs_path(f)
+        f_path = _get_abs_path(f)
 
-        if os.path.isdir(fpath):
-            call(['cp', '-a', fpath, folder_path])
-        elif os.path.isfile(fpath):
-            shutil.copy(fpath, folder_path)
+        if os.path.isdir(f_path):
+            call(['cp', '-a', f_path, folder_path])
+        elif os.path.isfile(f_path):
+            shutil.copy(f_path, folder_path)
 
     shutil.make_archive(archive_name, 'zip', folder_path)
-    check_archive_created(archive_name)
+    _check_archive_created(archive_name)
     shutil.rmtree(folder_path)
 
 
 def main():
     inputs = sys.argv[1:]
-    check_help_request(inputs)
+    _help_requested(inputs)
 
     if len(inputs) == 0:
-        ERROR("needs at least one argument being a folder name or a list of files/folders")
+        _error("needs at least one argument being a folder name or a list of files/folders")
         raise ValueError("no input ... no zip ...")
 
     elif len(inputs) == 1:
-        fpath = get_abs_path(inputs[0])
-        print(fpath)
-        if path_exists(fpath):
-            if os.path.isdir(fpath):
-                zip_folder(fpath)
+        f_path = _get_abs_path(inputs[0])
+        print(f_path)
+        if _path_exists(f_path):
+            if os.path.isdir(f_path):
+                _zip_folder(f_path)
             else:
-                zip_files([fpath])
+                _zip_files([f_path])
         else:
-            ERROR(CBBLUE + "%s" % fpath + CBASE + " path doesn't exist")
-            raise ValueError("needs at least one existing path in argument being a folder name or a list of files/folders")
+            _error(CBBLUE + "%s" % f_path + CBASE + " path doesn't exist")
+            raise ValueError(
+                "needs at least one existing path in argument being a folder name or a list of files/folders")
 
-    elif len(inputs) == 2 and not path_exists(get_abs_path(inputs[1])) and os.path.isdir(get_abs_path(inputs[0])):
-        folder_path = get_abs_path(inputs[0])
+    elif len(inputs) == 2 and not _path_exists(_get_abs_path(inputs[1])) and os.path.isdir(_get_abs_path(inputs[0])):
+        folder_path = _get_abs_path(inputs[0])
         archive_name = inputs[1]
-        zip_folder(folder_path, archive_name)
+        _zip_folder(folder_path, archive_name)
 
     else:
         last_arg = inputs[-1]
-        if path_exists(get_abs_path(last_arg)):
-            zip_files(inputs)
+        if _path_exists(_get_abs_path(last_arg)):
+            _zip_files(inputs)
         else:
-            zip_files(inputs[:-1], last_arg)
+            _zip_files(inputs[:-1], last_arg)
 
 
 if __name__ == "__main__":
